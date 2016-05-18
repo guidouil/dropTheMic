45b4846aadd669a493351196bdad03b7ac83a4d7
Guidouil <guidouil@gmail.com>
2016-05-18 16:53:15 +0200

    better muted

--- client/audio/audio_script.js
+++ client/audio/audio_script.js
@@ -1,3 +1,15 @@
+Template.audioBtn.onRendered(function(){
+  if (!Session.get('muted')) {
+    Session.set('muted', true);
+  }
+});
+
+Template.audio.onRendered(function () {
+  if (Session.equals('muted', false)) {
+    document.getElementById('audioPlayer').play();
+  }
+});
+
 Template.audio.helpers({
   isSoundMuted: function () {
     return Session.equals('muted', true);
@@ -6,10 +18,10 @@ Template.audio.helpers({
 
 Template.audioBtn.helpers({
   audioBtnImg: function () {
-    if (Session.equals('muted', true)) {
-      return 'unmute.png';
-    } else {
+    if (Session.equals('muted', false)) {
       return 'mute.png';
+    } else {
+      return 'unmute.png';
     }
   }
 });
@@ -18,6 +30,9 @@ Template.audioBtn.events({
   'click .audioBtn': function () {
     if (Session.equals('muted', true)) {
       Session.set('muted', false);
+      setTimeout(function () {
+        document.getElementById('audioPlayer').play();
+      }, 100);
     } else {
       Session.set('muted', true);
     }
